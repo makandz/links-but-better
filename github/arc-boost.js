4b3e09936f1f616cf591d85829a7e4da8344becc
Makan <makan@makan.bz>
2026-02-12 13:28:04 -0500

    fix: GitHub link copying (#2)

--- github/arc-boost.js
+++ github/arc-boost.js
@@ -1,24 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   const observer = new MutationObserver(() => {
-    const prTitleElement = document.querySelector(
-      ".js-issue-title.markdown-title"
-    );
-    const prIdElement = document.querySelector(
-      "bdi.js-issue-title.markdown-title + span"
-    );
+    const h1 =
+      document.querySelector('h1[data-component="PH_Title"]') ||
+      document.querySelector(".prc-PageHeader-Title-p0Mgh");
+
+    const prTitle = h1?.querySelector("span.markdown-title")?.innerText?.trim();
+    const prId = h1?.querySelector("span.pl-2")?.innerText?.trim() || "#????";
+
     const firstDivInSidebar = document.querySelector(
       "#partial-discussion-sidebar div"
     );
 
     if (
-      prTitleElement &&
+      prTitle &&
       firstDivInSidebar &&
-      prTitleElement.innerText &&
       !document.querySelector("#copy-pr-info")
     ) {
-      const prTitle = prTitleElement.innerText;
-      const prId = prIdElement?.innerText ?? "#????";
-
       const copyLink = document.createElement("a");
       copyLink.href = "#";
       copyLink.id = "copy-pr-info";
@@ -30,15 +27,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
       copyLink.addEventListener("click", (event) => {
         event.preventDefault();
+
+        const url = window.location.href;
+
         const clipboardData = [
           new ClipboardItem({
-            "text/plain": new Blob([window.location.href], {
-              type: "text/plain",
-            }),
+            "text/plain": new Blob([url], { type: "text/plain" }),
             "text/html": new Blob(
-              [
-                `:git_pr: <a href="${window.location.href}">${prTitle} (${prId})</a>`,
-              ],
+              [`:git_pr: <a href="${url}">${prTitle} (${prId})</a>`],
               { type: "text/html" }
             ),
           }),
